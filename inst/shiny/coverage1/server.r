f5b4a0868b2a224188aab27d51f77a5066f464ea
AndySouth <southandy@gmail.com>
2016-05-12 15:56:16 +0100

    start 1st shiny app

---
+++ inst/shiny/coverage1/server.r
@@ -0,0 +1,36 @@
+#coverage/inst/shiny/coverage1/server.r
+#andy south 12/5/16
+
+library(shiny)
+#install_github('AndySouth/coverage')
+library(coverage)
+
+
+shinyServer(function(input, output) {
+
+
+  output$plot_feed <- renderPlot({
+
+    #add dependency on the button
+    #if ( input$aButtonRun > 0 )
+    #{
+      #isolate reactivity of other objects
+    #  isolate({
+
+        #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
+
+        plot_feeding( man = input$feed_man,
+                      cow = input$feed_cow,
+                      indoor = input$feed_in,
+                      outdoor = input$feed_out )
+
+        #a hack to output the inputs
+        # cat("runcurtis_f2( max_gen=500",
+        #               "P_1 =",input$P_1,",",
+        #               ")\n" )
+
+      #}) #end isolate
+    #} #end if ( input$aButtonRun > 0 )
+  })
+
+})
---
+++ inst/shiny/coverage1/ui.r
@@ -0,0 +1,46 @@
+#coverage/inst/shiny/coverage1/ui.r
+#andy south 12/5/16
+
+library(shiny)
+
+
+shinyUI(fluidPage(
+
+  title = "coverage of vector control interventions",
+
+  h4("Coverage of vector control interventions is dependent on vector behaviour"),
+  h4("Vectors feed indoors and outdoors, on humans and cattle. Interventions target a subset of these behaviours."),
+  h4("Change inputs below to see the implications."),
+
+  fluidRow(
+    column(6, plotOutput('plotDefault'))
+    #column(6, plotOutput('plot'))
+  ), #end fluid row
+
+  hr(),
+
+  #h4("Curtis' input values are indicated in bold"),
+
+  fluidRow(
+    column(3,
+           h4("Vector feeding"),
+           numericInput("feed_man", "human", 0.7, min = 0, max = 1, step = 0.1),
+           numericInput("feed_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
+           numericInput("feed_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
+           numericInput("feed_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
+           #numericInput("P_2", "locus2 - proportion of locus1:", 0.1, min = 0.01, max = 100, step = 0.01)
+    ),
+    column(3, offset = 0,
+           h4("Intervention target"),
+           numericInput("target_man", "human", 0.7, min = 0, max = 1, step = 0.1),
+           numericInput("target_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
+           numericInput("target_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
+           numericInput("target_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
+    )
+
+  ), #end fluid row
+  fluidRow(
+    column(4, hr()),
+    column(2, actionButton('aButtonRun', 'Run Model'))
+  ) #end fluid row
+))
