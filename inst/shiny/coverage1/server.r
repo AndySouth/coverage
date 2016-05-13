dfcb2326e7769f2f27d75fe632faddad7b9c3d66
AndySouth <southandy@gmail.com>
2016-05-13 10:20:26 +0100

    remove cow & outdoor inputs, add interventions

--- inst/shiny/coverage1/server.r
+++ inst/shiny/coverage1/server.r
@@ -20,9 +20,11 @@ shinyServer(function(input, output, session) {
         #cat("running resistSimple with these inputs:", input$P_1, input$P_2*input$P_1, input$h.RS1_00, input$h.RS2_00,"\n")
 
         plot_feeding( man = input$feed_man,
-                      cow = input$feed_cow,
+                      cow = 1-input$feed_man,
                       indoor = input$feed_in,
-                      outdoor = input$feed_out )
+                      outdoor = 1-input$feed_in,
+                      intervention = input$intervention,
+                      coverage = input$target_coverage )
 
 
       #}) #end isolate
@@ -31,10 +33,10 @@ shinyServer(function(input, output, session) {
 
 
   #to update values based on changes in others
-  observe({ updateNumericInput(session, "feed_man", value = 1-input$feed_cow) })
-  observe({ updateNumericInput(session, "feed_cow", value = 1-input$feed_man) })
-  observe({ updateNumericInput(session, "feed_in", value = 1-input$feed_out) })
-  observe({ updateNumericInput(session, "feed_out", value = 1-input$feed_in) })
+  # observe({ updateNumericInput(session, "feed_man", value = 1-input$feed_cow) })
+  # observe({ updateNumericInput(session, "feed_cow", value = 1-input$feed_man) })
+  # observe({ updateNumericInput(session, "feed_in", value = 1-input$feed_out) })
+  # observe({ updateNumericInput(session, "feed_out", value = 1-input$feed_in) })
 
 
 })
--- inst/shiny/coverage1/ui.r
+++ inst/shiny/coverage1/ui.r
@@ -25,17 +25,20 @@ shinyUI(fluidPage(
     column(3,
            h4("Vector feeding"),
            numericInput("feed_man", "human", 0.7, min = 0, max = 1, step = 0.1),
-           numericInput("feed_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
-           numericInput("feed_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
-           numericInput("feed_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
+           #numericInput("feed_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
+           numericInput("feed_in","indoor", 0.6, min = 0, max = 1, step = 0.1)
+           #numericInput("feed_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
            #numericInput("P_2", "locus2 - proportion of locus1:", 0.1, min = 0.01, max = 100, step = 0.01)
     ),
     column(3, offset = 0,
-           h4("Intervention target"),
-           numericInput("target_man", "human", 0.7, min = 0, max = 1, step = 0.1),
-           numericInput("target_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
-           numericInput("target_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
-           numericInput("target_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
+           h4("Intervention"),
+           numericInput("target_coverage", "coverage", 0.7, min = 0, max = 1, step = 0.1),
+           radioButtons("intervention","intervention type",choices=c("bed nets","vet insecticide"))
+           # h4("Intervention target"),
+           # numericInput("target_man", "human", 0.7, min = 0, max = 1, step = 0.1),
+           # numericInput("target_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
+           # numericInput("target_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
+           # numericInput("target_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
     )
 
   ), #end fluid row
