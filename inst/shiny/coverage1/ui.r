63f24c9504895e01ade56b04fc87398c8d8d6a43
AndySouth <southandy@gmail.com>
2016-05-13 12:54:14 +0100

    rearrange inputs to single row

--- inst/shiny/coverage1/ui.r
+++ inst/shiny/coverage1/ui.r
@@ -15,7 +15,7 @@ shinyUI(fluidPage(
   fluidRow(
     column(8, plotOutput('plot_feed')),
     column(2, plotOutput('plot_pie_feed')),
-    column(2, plotOutput('plot_pie_exposure'))
+    column(2, plotOutput('plot_pie_expose'))
   ), #end fluid row
 
   hr(),
@@ -24,28 +24,30 @@ shinyUI(fluidPage(
 
   fluidRow(
     column(3,
-           h4("Vector feeding"),
-           sliderInput("feed_man", "human", 0.7, min = 0, max = 1, step = 0.1),
+           #h4("Vector feeding"),
+           sliderInput("feed_man", "vectors feeding on man", 0.7, min = 0, max = 1, step = 0.1)
            #numericInput("feed_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
-           sliderInput("feed_in","indoor", 0.6, min = 0, max = 1, step = 0.1)
+           #sliderInput("feed_in","indoor", 0.6, min = 0, max = 1, step = 0.1)
            #numericInput("feed_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
-           #numericInput("P_2", "locus2 - proportion of locus1:", 0.1, min = 0.01, max = 100, step = 0.01)
+    ),
+    column(3,
+           sliderInput("feed_in","vectors feeding indoors", 0.6, min = 0, max = 1, step = 0.1)
+    ),
+    column(2, offset = 0,
+           #h4("Intervention"),
+           radioButtons("intervention","intervention type",choices=c("bed nets","vet insecticide"))
+           #sliderInput("target_coverage", "coverage", 0.7, min = 0, max = 1, step = 0.1)
     ),
     column(3, offset = 0,
-           h4("Intervention"),
-           radioButtons("intervention","intervention type",choices=c("bed nets","vet insecticide")),
-           sliderInput("target_coverage", "coverage", 0.7, min = 0, max = 1, step = 0.1)
-
+           sliderInput("target_coverage", "intervention coverage", 0, min = 0, max = 1, step = 0.1)
+    )
            # h4("Intervention target"),
            # numericInput("target_man", "human", 0.7, min = 0, max = 1, step = 0.1),
            # numericInput("target_cow", "cattle", 0.3, min = 0, max = 1, step = 0.1),
            # numericInput("target_in","indoor", 0.6, min = 0, max = 1, step = 0.1),
            # numericInput("target_out","outdoor", 0.4, min = 0, max = 1, step = 0.1)
-    )
 
-  ), #end fluid row
-  fluidRow(
-    column(4, hr())
-    #column(2, actionButton('aButtonRun', 'Run Model'))
+
   ) #end fluid row
+
 ))
