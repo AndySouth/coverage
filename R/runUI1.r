b400f62741dc88e177fa623891fa781a6d22fc51
AndySouth <southandy@gmail.com>
2016-10-07 15:17:40 +0100

    function to run UI

---
+++ R/runUI1.r
@@ -0,0 +1,11 @@
+#' run an interactive shiny User Interface to the graphical model
+#'
+#' displays a user interface in the browser, allowing users to modify inputs
+
+#' @return nothing
+#' @export
+
+runUI1 <- function() {
+
+  shiny::runApp(system.file('shiny/coverage1', package='coverage'))
+}
